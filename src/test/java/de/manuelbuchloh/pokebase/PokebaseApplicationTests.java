c93a783c7a7f3d9629f9537e6f2e00b126180ca0
Manuel <mygamezapp@gmail.com>
2025-12-03 22:50:37 +0100

    adds a failing test

--- src/test/java/de/manuelbuchloh/pokebase/PokebaseApplicationTests.java
+++ src/test/java/de/manuelbuchloh/pokebase/PokebaseApplicationTests.java
@@ -3,11 +3,19 @@ package de.manuelbuchloh.pokebase;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Fail.fail;
+
 @SpringBootTest
 class PokebaseApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void failingTest() {
+		// absichtlich ein Fehler
+		fail("Dieser Test soll absichtlich fehlschlagen!");
 	}
 
 }
